58516d4cc695c951095fe55e23b5f37ba62ba7b2
Ryan Brayan <ryanbrayanf@gmail.com>
2022-03-05 15:40:51 -0300

    Anotaçoes

--- automaçao_cordenada/automacao_face_live.py
+++ automaçao_cordenada/automacao_face_live.py
@@ -8,24 +8,30 @@ log = open(r"C:\Users\USER\Desktop\dados_rafa\login.txt", "r")
 login = log.read()
 senha = login
 
-    
+    #currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse. currentMouseX, currentMouseY (1314, 345)
 def inicia_live():
-    try:
-        chrome = 'chrome.png'
-        pyautogui.click(chrome)
-    except:
-        chrome = 'chrome2.png'
-        pyautogui.click(chrome)
+    
+    sleep(3)
+    
+    chrome = 'automaçao_cordenada\chrome.png'
+    pyautogui.click(chrome)
+    sleep(3)
+        
     sleep(2)
     
     print('error2')
+    sleep(3)
     pyautogui.click(600, 600)
+    sleep(3)
     print('error1')
     pyautogui.write('https://www.youtube.com/')
     pyautogui.press(['Enter'])
     sleep(3)
     pyautogui.click('./abrir_tela.png')
-    
+    #pyautogui.locateOnScreen('looksLikeThis.png')  # returns (left, top, width, height) of first place it is found (863, 417, 70, 13)
+    #pyautogui.hotkey('ctrl', 'c')  # ctrl-c to copy
+    #for i in pyautogui.locateAllOnScreen('looksLikeThis.png')
+    #pyautogui.position()(187, 567)
     print('erro3')
     #write para escrever os codigos
     print('error')
