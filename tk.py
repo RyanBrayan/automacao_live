67602a3d4519b4e48e7c63f441736e1155503cc6
Ryan Brayan <ryanbrayanf@gmail.com>
2022-03-09 13:58:15 -0300

    Limpeza do codigo

--- tk.py
+++ tk.py
@@ -14,13 +14,11 @@ def botoes_tipo_culto():
     btn_salvar_ceia = Button(janela, text='Culto de Ceia', bg= 'white', activebackground= 'gray', font="Helvetica", command=lambda:janela_confirmacao_ceia(var, d, kw))
     btn_salvar_ceia.pack(pady=25, side= TOP)
 
-
     btn_salvar_normal = Button(janela, text='Culto normal', bg= 'white', activebackground= 'gray', font="Helvetica", command=lambda: janela_confirmacao_normal(var, d, kw))
     btn_salvar_normal.pack(pady=25, side= TOP)
 
     btn_salva_jovens = Button(janela, text='Culto de Jovens', bg= 'white', activebackground= 'gray', font="Helvetica", command=lambda: janela_confirmacao_jovens(var, d, kw))
     btn_salva_jovens.pack(pady=25, side= TOP)
-
 
 
 def janela_confirmacao_ceia(var, d, kw):
@@ -31,8 +29,6 @@ def janela_confirmacao_ceia(var, d, kw):
         print('ncdsdcnas')
         print('ndabbbcbcca,')
         janela.destroy()
-    else:
-        print('Você clicou em CANCELAR ou FECHOU a janela')
         
         
 def janela_confirmacao_normal(var, d, kw):
@@ -42,20 +38,17 @@ def janela_confirmacao_normal(var, d, kw):
         print('catapimbas')
         print('catabumba')
         janela.destroy()
-    else:
-        print('Você clicou em CANCELAR ou fechou a janela')
-        
+
+
 def janela_confirmacao_jovens(var, d, kw):
     msg = f'Voce apertou em Culto Jovens'
     resposta_jovens = messagebox.askokcancel(title='Confirmar ?', message=msg)
     if resposta_jovens is True:
         print('carambolas')
+        janela.destroy()
         
-    else:
-        print('Você clicou em CANCELAR ou fechou a janela')
      
         
-
 var = StringVar()
 d = StringVar()
 var2 = StringVar()
