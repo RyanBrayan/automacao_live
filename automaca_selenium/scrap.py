62c3199f38e80c231a57d970649a43de982cf6b8
Ryan Brayan <89818994+RyanBrayan@users.noreply.github.com>
2022-04-21 11:18:08 -0300

    Delete automaca_selenium directory

--- automaca_selenium/scrap.py
+++
@@ -1,7 +0,0 @@
-from tabula import read_pdf
-import pandas as pd
-
-arquivo = open('paes.txt', 'w')
-lendo = arquivo.readline()
-for c in arquivo:
-    print(c)
--- automaca_selenium/teste.py
+++
@@ -1,6 +0,0 @@
-#from datetime import date
-#data_atual = date.today()
-#print(data_atual.year)
-
-log = open(r"C:\Users\USER\Desktop\dados_rafa\login.txt", "r")
-print(log.read(14))
